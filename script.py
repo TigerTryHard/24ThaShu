7695b36a660d5745529f3678689841e6be6792e9
TigerTryHard <tiger.moodeng@gmail.com>
2025-02-04 20:38:16 +0700

    อัปเดตโค้ด

--- script.py
+++
@@ -1,8 +0,0 @@
-from browser import document, window
-
-def start_game(event):
-    document["message"].text = "เกมเริ่มต้นแล้ว!"
-    # เรียกฟังก์ชันจากไฟล์เกม 24code.py
-    window.start_24game()  # ฟังก์ชันที่ต้องมีใน 24code.py
-
-document["play"].bind("click", start_game)
